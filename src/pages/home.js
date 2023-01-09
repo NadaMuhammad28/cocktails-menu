a6d2f44cc1d5535951b95564bf501b4fceb807c8
Nadamuhammad28 <nadamuhammad28@gmail.com>
2023-01-09 05:06:56 +0200

    search done

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,5 @@
 import Search from "../components/search/search";
 import CocktailsList from "../components/cocktalsList/Cocktails";
-import Search from "../components/search/search";
 const Home = () => {
   return (
     <>
